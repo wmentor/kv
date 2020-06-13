88265268972f14f797fe788c1c5edf1240a1bda0
Mikhail Kirillov <wmentor@mail.ru>
2020-06-13 14:41:30 +0300

    first version

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/wmentor/lbase
+
+go 1.14
+
+require (
+	github.com/syndtr/goleveldb v1.0.0
+	github.com/wmentor/dsn v1.0.1
+)
---
+++ lbase.go
@@ -0,0 +1,96 @@
+package lbase
+
+import (
+	"errors"
+
+	"github.com/wmentor/dsn"
+
+	"github.com/syndtr/goleveldb/leveldb"
+	"github.com/syndtr/goleveldb/leveldb/opt"
+)
+
+type DB interface {
+	Delete(key []byte)
+	Get(key []byte) []byte
+	Has(key []byte) bool
+	Set(key, value []byte)
+	Close()
+}
+
+type db struct {
+	base *leveldb.DB
+}
+
+var (
+	ErrNoPath error = errors.New("db path not entered")
+)
+
+func Open(params string) (DB, error) {
+
+	kvs, err := dsn.New(params)
+	if err != nil {
+		return nil, err
+	}
+
+	path := kvs.GetString("path", "")
+	if path == "" {
+		return nil, ErrNoPath
+	}
+
+	base, err := leveldb.OpenFile(path, &opt.Options{
+		CompactionTableSize: 16,
+		WriteBuffer:         16 * 2,
+		Compression:         opt.SnappyCompression,
+		ReadOnly:            false,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	ldb := &db{
+		base: base,
+	}
+
+	return ldb, nil
+}
+
+func (base *db) Close() {
+	if base != nil && base.base != nil {
+		base.base.Close()
+		base.base = nil
+	}
+}
+
+func (base *db) Get(key []byte) []byte {
+
+	if base != nil && base.base != nil {
+		if len(key) != 0 {
+			if val, err := base.base.Get(key, nil); err == nil {
+				return val
+			}
+		}
+	}
+
+	return nil
+}
+
+func (base *db) Has(key []byte) bool {
+	return len(base.Get(key)) > 0
+}
+
+func (base *db) Delete(key []byte) {
+	base.Set(key, nil)
+}
+
+func (base *db) Set(key, value []byte) {
+	if base != nil && base.base != nil {
+		if len(key) != 0 {
+			if len(value) == 0 {
+				base.base.Delete(key, nil)
+			} else {
+				base.base.Put(key, value, nil)
+			}
+		}
+	}
+}
